97e0343c28d7bdd121943d5992ef49385d84b858
Ricardo Coronel <montotox@gmail.com>
2021-10-23 01:49:14 +0200

    finish challenge

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# weventure_challenge
+# WEVENTURE Coding Challenge
+
+Visit the [Demo](https://instotox.netlify.app).
+
+## Starting 🚀
+
+_This is instructions permit you to obtain a copy for this project._
+
+### Pre-requirements 📋
+
+1. Clone this project
+
+```
+$ git clone https://github.com/montotox/weventure_challenge
+```
+
+### Installation 🔧
+
+1. Enter in your folder .../weventure_challenge/
+2. Open _index.html_
+3. The data is stored in localStorage
+4. If you want to reset the app, you would clear localStorage
+
+## Autors ✒️
+
+- **Ricardo Coronel** - _Initial work / Documentation_ - [montotox](https://github.com/montotox)
+
+## Expressions of gratitude 🎁
+
+- Comment with others about this project 📢
+- Invite a beer 🍺 or a coffe ☕.
+- Give thanks publicly 🤓.
+- I'm coding with ❤️.
+
+---
---
+++ js/main.js
@@ -0,0 +1,103 @@
+// Tasks array
+let initialTasks = [
+  "Get up early",
+  "Do some meditation",
+  "Eat a healthy breakfast",
+  "Drink plenty of water",
+  "Exercise",
+  "Smile :)",
+];
+// States array
+let initialStates = [
+  "check",
+  "check",
+  "uncheck",
+  "uncheck",
+  "uncheck",
+  "uncheck",
+];
+
+// Get items from localStorage
+if (localStorage.getItem("items")) {
+  var tasks = JSON.parse(localStorage.getItem("items"));
+} else {
+  var tasks = initialTasks;
+}
+
+// Get states from localStorage
+if (localStorage.getItem("states")) {
+  var states = JSON.parse(localStorage.getItem("states"));
+} else {
+  var states = initialStates;
+}
+
+// Get the app element
+let app = document.querySelector("#list");
+
+// Create markup
+app.innerHTML = tasks
+  .map((task, i) => {
+    return (
+      '<li class="' +
+      states[i] +
+      '"><p>' +
+      task +
+      '</p><span><button type="button" class="btn">Delete</button></span></li>'
+    );
+  })
+  .join("");
+
+// Create new task
+$("input").keypress(function (event) {
+  if (event.which === 13) {
+    var toDoText = $(this).val();
+    $(this).val("");
+    $("ul").append(
+      '<li class="uncheck"><p>' +
+        toDoText +
+        '</p><span><button type="button" class="btn">Delete</button></span></li>'
+    );
+    writeStorage();
+  }
+});
+
+// Delete task
+$("ul").on("click", "span", function () {
+  $(this)
+    .parent()
+    .fadeOut(500, function () {
+      $(this).remove();
+      writeStorage();
+    });
+});
+
+// Delete all tasks
+$("#clr").click(function () {
+  $("li").remove();
+  writeStorage();
+});
+
+// Check task
+$("ul").on("click", "li", function () {
+  if ($(this).hasClass("uncheck")) {
+    $(this).removeClass("uncheck").addClass("check");
+    writeStorage();
+  } else {
+    $(this).removeClass("check").addClass("uncheck");
+    writeStorage();
+  }
+});
+
+// Convert HTML elements to Array -> JSON & Write localStorage
+var writeStorage = function () {
+  let items = [];
+  let classes = [];
+  let list = document.getElementById("list").getElementsByTagName("li");
+  [].forEach.call(list, (element) => {
+    let text = element.childNodes[0].innerText;
+    items.push(text);
+    classes.push(element.className);
+  });
+  localStorage.setItem("items", JSON.stringify(items));
+  localStorage.setItem("states", JSON.stringify(classes));
+};
